25d3ff28bb376d0355aa751f517e19c71bd29695
Andrey Smakotin <slamfromm@gmail.com>
2021-10-21 20:30:41 +0300

    waitress

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from urls import get_view
 
 
 def application(env, start_response):
-    print(env)
     http_cookie = env['HTTP_COOKIE']
     raw_uri = env['PATH_INFO']
     request_method = env['REQUEST_METHOD']
@@ -10,6 +9,7 @@ def application(env, start_response):
     if request_method == "POST":
         content_length = int(env['CONTENT_LENGTH'])
         body = env['wsgi.input'].read(content_length)
-    status_code, headers, view = get_view(raw_uri, request_method, http_cookie, body)
+    response_from_get_view = get_view(raw_uri, request_method, http_cookie, body)
+    status_code, headers, view = response_from_get_view
     start_response(status_code, [('Content-Type', 'text/html')] + headers)
     return [view]
--- urls.py
+++ urls.py
@@ -12,4 +12,4 @@ def get_view(raw_uri, request_method, http_cookie, body):
     for regex_pattern, view_fun in urlpatterns:
         if pattern := re.match(regex_pattern, raw_uri):
             return view_fun(request_method, http_cookie, body, **pattern.groupdict())
-    return 404
+    return "404 not found", [], b""
